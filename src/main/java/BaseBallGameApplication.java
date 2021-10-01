49069b17f4f0956561f420fac1f06b167887afa6
JanghoPark <pjhsk113@naver.com>
2021-10-01 20:09:52 +0900

    Initial commit

---
+++ src/main/java/BaseBallGameApplication.java
@@ -0,0 +1,8 @@
+import baseball.domain.BaseBallNumber;
+import baseball.view.InputView;
+
+public class BaseBallGameApplication {
+    public static void main(String[] args) {
+        BaseBallNumber baseBallNumber = InputView.enterNumber();
+    }
+}
